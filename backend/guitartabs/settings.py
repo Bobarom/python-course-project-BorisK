41e5bdc749ba53bee26d0d3220acb4c05037e45d
Bobarom <bobarkumanov@gmail.com>
2026-05-31 12:38:31 +0300

    migrated db to postgresql

--- backend/guitartabs/settings.py
+++ backend/guitartabs/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'guitartabs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tab_db',
+        'USER': 'guitar_user',
+        'PASSWORD': 'i_love_tabs6487',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
